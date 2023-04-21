193fef0272168d381d498fedea3c2f05a1a6b668
Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>
2023-04-21 08:04:25 -0700

    sync
    Signed-off-by: Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
+import NextLink from 'next/link'
 import {
-    Container,
-    Box,
-    Button,
-    Heading,
-    Image,
     Link,
+    Container,
+    Heading,
+    Box,
+    SimpleGrid,
+    Button,
+    List,
+    ListItem,
     useColorModeValue,
     chakra
 } from '@chakra-ui/react'
-import { ChevronRightIcon } from '@chakra-ui/icons'
-import NextLink from 'next/link'
-import Section from '../components/section'
+import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+import Image from 'next/image'
 
 const ProfileImage = chakra(Image, {
     shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
@@ -112,6 +118,92 @@ const Page = () => {
                     <BioYear>2012 to present</BioYear>
                     Working as a freelancer
                 </BioSection>
+            </Section>
+
+            <Section delay={0.3}>
+                <Heading as="h3" variant="section-title">
+                    On the web
+                </Heading>
+                <List>
+                    <ListItem>
+                        <Link
+                            href="https://github.com/craftzdog"
+                            target="_blank"
+                        >
+                            <Button
+                                variant="ghost"
+                                colorScheme="teal"
+                                leftIcon={<IoLogoGithub />}
+                            >
+                                @craftzdog
+                            </Button>
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Link
+                            href="https://twitter.com/inkdrop_app"
+                            target="_blank"
+                        >
+                            <Button
+                                variant="ghost"
+                                colorScheme="teal"
+                                leftIcon={<IoLogoTwitter />}
+                            >
+                                @inkdrop_app (English)
+                            </Button>
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Link
+                            href="https://twitter.com/craftzdog"
+                            target="_blank"
+                        >
+                            <Button
+                                variant="ghost"
+                                colorScheme="teal"
+                                leftIcon={<IoLogoTwitter />}
+                            >
+                                @craftzdog (日本語)
+                            </Button>
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Link
+                            href="https://instagram.com/craftzdog"
+                            target="_blank"
+                        >
+                            <Button
+                                variant="ghost"
+                                colorScheme="teal"
+                                leftIcon={<IoLogoInstagram />}
+                            >
+                                @craftzdog
+                            </Button>
+                        </Link>
+                    </ListItem>
+                </List>
+
+                <SimpleGrid columns={[1, 2, 2]} gap={6}></SimpleGrid>
+
+                <Heading as="h3" variant="section-title">
+                    Newsletter
+                </Heading>
+                <p>
+                    Join me on a behind-the-scenes coding journey. Weekly
+                    updates on projects, tutorials, and videos
+                </p>
+
+                <Box align="center" my={4}>
+                    <Button
+                        as={NextLink}
+                        href="https://www.devas.life/"
+                        scroll={false}
+                        leftIcon={<EmailIcon />}
+                        colorScheme="teal"
+                    >
+                        Sign up my newsletter here
+                    </Button>
+                </Box>
             </Section>
         </Container>
     )
