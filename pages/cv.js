adb1cdf2841b4e7de10f22c9077f070413aab6b4
Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>
2023-04-25 20:09:15 -0700

    add icon
    Signed-off-by: Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>

--- pages/cv.js
+++ pages/cv.js
@@ -25,7 +25,7 @@ import {
     RefAff2
 } from '../components/bio'
 
-import { RxExternalLink } from 'react-icons/Rx'
+import { RxExternalLink } from 'react-icons'
 
 const CV = () => (
     <Layout title="CV">
